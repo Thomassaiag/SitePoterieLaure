9df84f73529f18b0f968d8ec9ff3b17b6fc341ee
Thomassaiag <thomas.saiag@gmail.com>
2023-12-22 17:01:38 +0100

    Import Questrial font + created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='footerClass'>
+        <span className='imageSpan_Class'>
+            <img href="" alt="instagram"></img>
+            <img href="" alt="facebook"></img>
+            <img href="" alt="instagram"></img>
+        </span>
+    </footer>
+  )
+}
