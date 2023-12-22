d2ff0d0c389b3b3772bc6ebfd023aab3995491a1
Thomassaiag <thomas.saiag@gmail.com>
2023-12-22 17:51:17 +0100

    updated style on header, website logo as a background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
+let copyrightYear=2023
+
 export const Footer = () => {
   return (
     <footer className='footerClass'>
-        <span className='imageSpan_Class'>
+        <span className='footerImageSpan_Class'>
             <img href="" alt="instagram"></img>
             <img href="" alt="facebook"></img>
             <img href="" alt="instagram"></img>
         </span>
+        <span className='footerTextSpan_Class'>
+            <p>Condition Générales de Vente</p>
+            <p>/</p>
+            <p>Mentions Légales</p>
+            <p>/</p>
+            <p>Emballages et Livraisons</p>
+            <p>/</p>
+            <p>Confidentialité</p>
+        </span>
+        <p>@VL Céramique {copyrightYear}. Powered by Tom</p>
     </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,13 @@ export const Header = () => {
   return (
     <div>
       <button className='connectionButton_Class'>Se Connecter</button>
-      <p className='nameTitle_Class'>Laure Videau</p>
-      <p className='subTitle_Class'>céramique</p>
-      <img src="" alt='webSiteLogo'/>
+      <div className='textImageContainer_class'>
+        <div className='titleDiv_Class'>
+          <p className='nameTitle_Class'>Laure Videau</p>
+          <p className='subTitle_Class'>céramique</p>
+        </div>
+        <img className='webSiteLogo_Class'src="" alt='webSiteLogo'/>
+      </div>
       <span className='links_Class'>
         <a href=''>Collections</a>
         <a href=''>Boutique</a>
