af7aa6f68732e63d4d2b94762ac63617ecbfccc6
Thomassaiag <thomas.saiag@gmail.com>
2023-12-22 16:26:26 +0100

    Title text + link texts

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <p className='NameTitle'>Laure Videau</p>
+      <img src="" alt='WebSiteLogo'/>
+      <span className='linksClass'>
+        <a href=''>Collections</a>
+        <a href=''>Boutique</a>
+        <a href=''>Galerie</a>
+        <a href=''>Blog</a>
+        <a href=''>Contact</a>
+      </span>
+
+
+    </div>
+  )
+}
