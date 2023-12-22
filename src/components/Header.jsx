e18357e5d1e46f86f524176099fe60861e9d0272
Thomassaiag <thomas.saiag@gmail.com>
2023-12-22 16:32:47 +0100

    Se connecter button, top right corner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export const Header = () => {
   return (
     <div>
-      <p className='NameTitle'>Laure Videau</p>
-      <img src="" alt='WebSiteLogo'/>
-      <span className='linksClass'>
+      <button className='connectionButton_Class'>Se Connecter</button>
+      <p className='NameTitle_Class'>Laure Videau</p>
+      <img src="" alt='webSiteLogo'/>
+      <span className='links_Class'>
         <a href=''>Collections</a>
         <a href=''>Boutique</a>
         <a href=''>Galerie</a>
