864771aad42c56216d7191a50b9078a1b5729729
tsaiag <tsaiag@groupeseb.com>
2024-02-20 13:16:34 +0100

    updated CSS for footer & header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,23 +13,19 @@ export const Header = () => {
       </div>
       <div className='headerLogoDiv_Class'>  
         <Link to='/'>
-          
-            <div className='titleDiv_Class'>
-              <img className='webSiteLogo_Class'src={logoLaureSansNom} alt='webSiteLogo'/>
-              <p className='nameTitle_Class'>Laure Videau</p>
+            <span className='headerNameTitle_Class'>Laure Videau</span>
+            <div className='headerTitleDiv_Class'>
+              <img className='headerWebSiteLogo_Class'src={logoLaureSansNom} alt='webSiteLogo'/>
             </div>
-          
         </Link>
       </div>
-      <div>  
-        <div className='headerLinks_Class'>
-          <Link to='/collections'>Collections</Link>
-          <Link to=''>Boutique</Link>
-          <Link to='/galerie'>Galerie</Link>
-          <Link to='/portrait'>Portrait</Link>
-          <Link to=''>Blog</Link>
-          <Link to=''>Contact</Link>
-        </div>
+      <div className='headerLinks_Class'>
+        <Link to='/collections'>Collections</Link>
+        <Link to=''>Boutique</Link>
+        <Link to='/galerie'>Galerie</Link>
+        <Link to='/portrait'>Portrait</Link>
+        <Link to=''>Blog</Link>
+        <Link to=''>Contact</Link>
       </div>
     </div>
   )
