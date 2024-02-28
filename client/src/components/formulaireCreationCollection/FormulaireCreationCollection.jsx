7389eac9a0c45a4e10de5c4e10004c5e03cf7823
tsaiag <tsaiag@groupeseb.com>
2024-02-28 17:06:25 +0100

    creation of 2 fields collectionName and CollectionDescription

--- client/src/components/formulaireCreationCollection/FormulaireCreationCollection.jsx
+++ client/src/components/formulaireCreationCollection/FormulaireCreationCollection.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const FormulaireCreationCollection = () => {
+    const [collectionName, setCollectionName]=useState([null])
+    const [collectionDescription, setCollectionDescription]=useState([null])
+
+
+    const handleSubmit=async(event)=>{
+        event.preventDefault()
+        try {
+            const response=await fetch('/admin',{
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({collectionName})
+            })
+            if(!response.ok){
+                throw new Error('Network response was not OK')
+            }
+            else console.log('New Entry Created Successfuly')
+        } catch (error) { 
+        }
+    }
   return (
     <div>
          <h1>Créer une nouvelle Collection</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Nom de la Collection</label> 
-                <input></input>
+                <input
+                    type="text"
+                    id="collectionName"
+                    value={collectionName}
+                    onChange={(e)=>setCollectionName(e.target.value)}
+                />
             </div>
             <div>
                 <label>Description de la Collection</label> 
-                <input></input>
+                <input
+                    type='text'
+                    id='collectionDescription'
+                    value={collectionDescription}
+                    onChange={(e)=>setCollectionDescription(e.target.value)}
+                />
             </div>
+            <button type='submit'>Créer Collection</button>
         </form>
     </div>
   )
