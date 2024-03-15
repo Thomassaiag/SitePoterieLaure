76fb46a97d3a54730e21e939a14ead4d4f4b1f74
Thomassaiag <thomas.saiag@gmail.com>
2024-03-15 19:30:30 +0100

    created PopUpPicture component and logic

--- client/src/components/collectionElementPicture/CollectionElementPicture.jsx
+++ client/src/components/collectionElementPicture/CollectionElementPicture.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './CollectionElementPicture.css'
+import { PopUpPicture } from '../popUpPicture/PopUpPicture'
 
 export const CollectionElementPicture = ({collection_element_picture_url, collection_element_picture_alt}) => {
+  const [showPopUpPicture, setShowPopUpPicture]=useState(false)
+  const togglePopUpPicture=()=>{
+    setShowPopUpPicture(!showPopUpPicture)
+  }
+
+
+
   return (
     <div className='collectionPictureContainer'>
-      <img src={collection_element_picture_url} alt={collection_element_picture_alt} />
+      <img src={collection_element_picture_url} alt={collection_element_picture_alt} onClick={togglePopUpPicture}/>
+      {showPopUpPicture && <PopUpPicture imageUrl={collection_element_picture_url} imageAlt={collection_element_picture_alt} onClose={togglePopUpPicture}/>}
     </div>
   )
 }
--- client/src/components/popUpPicture/PopUpPicture.jsx
+++ client/src/components/popUpPicture/PopUpPicture.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './PopUpPicture.css'
 
-export const PopUpPicture = ({imageUrl, imageAlt}) => {
+export const PopUpPicture = ({imageUrl, imageAlt, onClose}) => {
   return (
-    <div className='popUpPictureContainer'>PopUpPicture
-        <img src={imageUrl} alt={imageAlt}/>
-        <img src='images/leftChevron.jpg' alt='previous picture'/>
-        <img src='images/rightChevron.jpg' alt='next picture'/>
-    </div>
+    
+      <div className='overlay'>
+        <div className='popup'>
+          <div className='chevronPictureContainer'>
+            <img src='/images/leftChevron.jpg' alt='previous picture'/>
+          </div>
+          <img className='popUpPicture' src={imageUrl} alt={imageAlt} onClick={onClose}/>
+          <div className='chevronPictureContainer'>
+            <img src='/images/rightChevron.jpg' alt='next picture'/>
+          </div>
+        </div>
+      </div>
+    
   )
 }
