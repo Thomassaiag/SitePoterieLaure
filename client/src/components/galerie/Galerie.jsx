1dc18984a4ac7d1642f1ac7e70ab29bc9ce30c4c
tsaiag <tsaiag@groupeseb.com>
2024-03-20 17:14:29 +0100

    Updated Galerie Style

--- client/src/components/galerie/Galerie.jsx
+++ client/src/components/galerie/Galerie.jsx
@@ -6,7 +6,7 @@ import './Galerie.css'
 
 export const Galerie = () => {
   return (
-    <div className='pictureContainer'>
+    <div className='picturesContainer'>
         {imagesGalerie.map(image=>{
             return(
                 <img key={image.id} src={image.imageUrl} alt={image.imageAlt}/>
