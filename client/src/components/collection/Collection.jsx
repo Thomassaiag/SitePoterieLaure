c48ef992a2dec50c21cdd6f6f128abc069a95c3e
Thomassaiag <thomas.saiag@gmail.com>
2024-05-16 22:06:28 +0200

    creation of delete button when admin is on

--- client/src/components/collection/Collection.jsx
+++ client/src/components/collection/Collection.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './Collection.css'
 import { CollectionMainPic } from '../collectionMainPic/CollectionMainPic'
+import { useConnectionStatus } from '../contextProvider/ConnectionStatusContextProvider'
+// import {deleteCollection} from '../../../public/images/deleteCollection.jpg'
 
 export const Collection = ({imageUrl, imageAlt, title}) => {
+
+  const {connectionAttributes}=useConnectionStatus()
+
+  const deleteCollection=async()=>{
+    try {
+      const response=await fetch('http://localhost/collections/admin/deleteCollection/:id')
+      const data=response.json()
+    } catch (error) {
+      
+    }
+  }
+
+
   return (
     <div className='collectionContainer'>
         <CollectionMainPic imageUrl={imageUrl} imageAlt={imageAlt}/>
         <p>{title}</p>
+        {connectionAttributes.adminConnection && <img className='deleteCollectionButton' src="../../../public/images/deleteCollection.jpg" alt="Delete Collection" onClick={deleteCollection}/>}
+        
     </div>
   )
 }
