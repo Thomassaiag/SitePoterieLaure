9559620d2922296d252d57551242d4d467370cef
tsaiag <tsaiag@groupeseb.com>
2024-05-22 15:58:27 +0200

    Update form can caputre updated information

--- client/src/components/updateElementInformation/UpdateElementInformation.jsx
+++ client/src/components/updateElementInformation/UpdateElementInformation.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 export const UpdateElementInformation = ({collectionElementDescription, collectionElementEmail, collectionElementCooking, collectionElementRecommandation}) => {
 
@@ -9,30 +9,50 @@ export const UpdateElementInformation = ({collectionElementDescription, collecti
       collectionElementRecommandationToUpdate: collectionElementRecommandation
     })
 
-    const UpdateCollectionElementInformation=()=>{
+    const handleSubmit=()=>{
       return
     }
 
+
+    const updateCollectionElementInformation=(e)=>{
+      e.preventDefault()
+      SetcollectionInformationToUpdate({...collectionInformationToUpdate,
+        [e.target.name]:e.target.value
+      })
+    }
+
+    useEffect(()=>{
+      console.log(collectionInformationToUpdate)
+    },[collectionInformationToUpdate])
+
     return (
       <div>UpdateElementInformation
-        <form onSubmit={UpdateCollectionElementInformation}>
+        <form onSubmit={handleSubmit}>
           <label
             htmlFor='collectionElementDescription'>Collection Description  
           </label>
           <input id='collectionElementDescription'
-            placeholder={collectionElementDescription}        
+            placeholder={collectionElementDescription}
+            onChange={updateCollectionElementInformation}
+            name='collectionElementDescriptionToUpdate'        
           />
           <label
             htmlFor='collectionElementEmail'>Informations techniques  
           </label>
           <input id='collectionElementEmail'
-            value={collectionElementEmail}        
+            value={collectionElementEmail}
+            onChange={updateCollectionElementInformation}
+            name='collectionElementEmailToUpdate'    
           />
           <input id='collectionElementCooking'
-            placeholder={collectionElementCooking}        
+            placeholder={collectionElementCooking}
+            onChange={updateCollectionElementInformation}
+            name='collectionElementCookingToUpdate'        
           />
           <input id='collectionElementRecommandation'
-            placeholder={collectionElementRecommandation}        
+            placeholder={collectionElementRecommandation}
+            onChange={updateCollectionElementInformation}
+            name='collectionElementRecommandationToUpdate'        
           />
           <button>Update Collection</button>
 
