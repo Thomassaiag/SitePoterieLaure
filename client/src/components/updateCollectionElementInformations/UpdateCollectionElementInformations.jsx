132cbd3631469c7e594bfcc84f447d336c9b00ce
Thomassaiag <thomas.saiag@gmail.com>
2024-05-24 13:23:14 +0200

    capturing updated information into element information form

--- client/src/components/updateCollectionElementInformations/UpdateCollectionElementInformations.jsx
+++ client/src/components/updateCollectionElementInformations/UpdateCollectionElementInformations.jsx
@@ -7,45 +7,72 @@ export const UpdateCollectionElementInformations = ({collection_uid}) => {
 
     const {currentInformations}=useCollectionElementInformations()
     const [currentInformationsToUpdate, setCurrentInformationToUpdate]=useState(currentInformations)
+    // const [currentInformationToUpdateText,setCurrentInformationToUpdateText]=useState()
 
-    const updateCollectionElementInformations=(e)=>{
+    const updateCollectionElementInformations=(e, informationId)=>{
         e.preventDefault()
-        console.log(e.target.name)
+        // console.log(e.target.name)
         setCurrentInformationToUpdate((prevInformations)=>{
-            console.log("test")
-            console.log("prevInformations =>",prevInformations)
+            // console.log("prevInformations =>",prevInformations)
+            // console.log(e.target.value)
             return prevInformations.map((prevInformation)=>{
-                let {collection_element_information_text, collection_element_information_uid}=prevInformation
-                console.log(parseInt(collection_element_information_uid)==parseInt(e.target.name))
-                console.log(e.target.value)
+                // console.log(prevInformation)
+                let {collection_element_information_uid}=prevInformation
                 if(parseInt(collection_element_information_uid)==parseInt(e.target.name)){
-                    return {...prevInformation, [collection_element_information_text]: e.target.value}
+                    return {...prevInformation, collection_element_information_text: e.target.value}
                 } else {
                     return prevInformation
                 }  
-            })
+            }) 
         })
-        return
+        // setCurrentInformationToUpdateText((prevCurrentInformationsToUpdate)=>{
+        //     if (prevCurrentInformationsToUpdate){
+
+        //         let elementInformation = prevCurrentInformationsToUpdate.find((collectionElementInformation)=>collectionElementInformation.collection_element_information_uid===informationId)
+        //         console.log(elementInformation)
+        //         return elementInformation!==undefined ? elementInformation.collection_element_information_text : undefined
+        //     }
+        // })
+        // e.target.value=currentInformationToUpdateText
     }
 
+
     useEffect(()=>{
-        setCurrentInformationToUpdate(currentInformations)
+        if(currentInformations.length>0){
+            setCurrentInformationToUpdate(currentInformations)
+        }
     },[currentInformations])
 
+    useEffect(()=>{
+        console.log(currentInformationsToUpdate)
+    },[currentInformationsToUpdate])
 
+
+    // const findInformationTextForInformationUID=(e,id, defaultValue)=>{
+    //     e.preventDefault()
+    //     if(currentInformationsToUpdate){
+    //         let informationElement=currentInformationsToUpdate.find((currentInformation)=>currentInformation.collection_element_information_uid==id)
+    //         console.log(informationElement.collection_element_information_text)
+    //         return informationElement.collection_element_information_text
+    //     } else {
+    //         console.log(defaultValue)
+    //         return defaultValue
+    //     } 
+    // }
 
   return (
     <div className='collectionElementInformationsContainer'>
     {
-        currentInformations ?(
-            currentInformations.map((currentInformation)=>{
+        currentInformationsToUpdate ?(
+            currentInformationsToUpdate.map((currentInformation)=>{
                 let {collection_element_information_text, collection_element_information_uid}=currentInformation
                 return(
                     <input
                         name={collection_element_information_uid}
                         key={collection_element_information_uid}
+                        // value={(e)=>findInformationTextForInformationUID(e,collection_element_information_uid, collection_element_information_text)}
                         value={collection_element_information_text}
-                        onChange={updateCollectionElementInformations}
+                        onChange={(e)=>updateCollectionElementInformations(e,collection_element_information_uid)}
                     />
                 )
             })
