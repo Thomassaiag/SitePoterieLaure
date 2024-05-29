ac1e1a178ef6744d3f7cc51900f0fb0e27c2a99d
Thomassaiag <thomas.saiag@gmail.com>
2024-05-29 22:46:41 +0200

    ccreating onchange logic on dynamic form

--- client/src/components/collectionElementInformationsCreation/CollectionElementInformationsCreation.jsx
+++ client/src/components/collectionElementInformationsCreation/CollectionElementInformationsCreation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {nanoid} from  'nanoid'
 
 export const CollectionElementInformationsCreation = () => {
@@ -6,40 +6,56 @@ export const CollectionElementInformationsCreation = () => {
 
     const [inputIDList, setInputIDList]=useState([{id:nanoid()}])
 
-    const [collectionInformationsToCreate, setCollectionInformationsToCreate]=useState([])
+    const [collectionInformationsToCreate, setCollectionInformationsToCreate]=useState([{
+        informationInputId:inputIDList[0].id,
+        informationInputText:''
+    }])
 
     const addNewInformation=(e)=>{
         e.preventDefault()
         setInputIDList(prevInputIDList=>[...prevInputIDList,{id:nanoid()}])
     }
-
+    
     const deleteInformation=(e,idToDelete)=>{
         e.preventDefault()
         setInputIDList(prevInputIDList=>prevInputIDList.filter(prevInputID=>prevInputID.id!=idToDelete))
+        setCollectionInformationsToCreate(prevCollectionInformationsToCreate=>prevCollectionInformationsToCreate.filter(prevCollectionInformationToCreate=>prevCollectionInformationToCreate.informationInputId!=idToDelete))
     }
-
-
-    const handleChange=(e)=>{
+    
+    
+    const handleChange=(e,inputId)=>{
         e.preventDefault()
-        setCollectionInformationsToCreate((collectionInformationsToCreate)=>{
-            return collectionInformationsToCreate.map((collectionInformations)=>{
-                if(collectionInformations.uid)
-                return {...collectionInformations}
+        setCollectionInformationsToCreate((prevCollectionInformationsToCreate)=>{
+            return prevCollectionInformationsToCreate.map((collectionInformationToCreate)=>{
+                const {informationInputId,informationInputText}=collectionInformationToCreate
+                if(informationInputId===inputId){
+                    return {...collectionInformationToCreate,informationInputText:e.target.value}
+                } else {
+                    return [...prevCollectionInformationsToCreate,{informationInputId:inputId, informationInputText:e.target.value}]
+                }
             })
         })
     }
 
+    useEffect(()=>{
+        console.log(collectionInformationsToCreate)
+    },[collectionInformationsToCreate])
+
+        useEffect(()=>{
+        console.log(collectionInformationsToCreate)
+    },[])
 
     return (
         <div>
-            {inputIDList.map((element)=>{
-                const{id}=element
+            {inputIDList.map((inputID)=>{
+                const{id}=inputID
                 return (
                     <div key={id}>
                     <input 
                         type='text'
                         name={id}
-                        onChange={handleChange}
+                        onChange={(e)=>handleChange(e,id)}
+                        value={collectionInformationsToCreate.informationInputText}
                     />
                     <button type='button' onClick={(e)=>deleteInformation(e, id)}>Delete Information</button>
                     </div>
