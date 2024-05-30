fa14ceab8905caf2c472b2882c8663f8f5fe6f94
tsaiag <tsaiag@groupeseb.com>
2024-05-30 09:25:36 +0200

    dynamic input information capture ok, next step => create context provider and use is in end point

--- client/src/components/collectionElementInformationsCreation/CollectionElementInformationsCreation.jsx
+++ client/src/components/collectionElementInformationsCreation/CollectionElementInformationsCreation.jsx
@@ -18,22 +18,36 @@ export const CollectionElementInformationsCreation = () => {
     
     const deleteInformation=(e,idToDelete)=>{
         e.preventDefault()
-        setInputIDList(prevInputIDList=>prevInputIDList.filter(prevInputID=>prevInputID.id!=idToDelete))
-        setCollectionInformationsToCreate(prevCollectionInformationsToCreate=>prevCollectionInformationsToCreate.filter(prevCollectionInformationToCreate=>prevCollectionInformationToCreate.informationInputId!=idToDelete))
+        if(inputIDList.length==1){
+           alert("vous devez avoir au moins 1 information")
+        } else {
+            setInputIDList(prevInputIDList=>prevInputIDList.filter(prevInputID=>prevInputID.id!=idToDelete))
+            setCollectionInformationsToCreate(prevCollectionInformationsToCreate=>prevCollectionInformationsToCreate.filter(prevCollectionInformationToCreate=>prevCollectionInformationToCreate.informationInputId!=idToDelete))
+        }
     }
     
     
     const handleChange=(e,inputId)=>{
         e.preventDefault()
+        console.log("inputID =>",inputId)
+        console.log("input value => ",e.target.value)
         setCollectionInformationsToCreate((prevCollectionInformationsToCreate)=>{
-            return prevCollectionInformationsToCreate.map((collectionInformationToCreate)=>{
-                const {informationInputId,informationInputText}=collectionInformationToCreate
-                if(informationInputId===inputId){
-                    return {...collectionInformationToCreate,informationInputText:e.target.value}
-                } else {
-                    return [...prevCollectionInformationsToCreate,{informationInputId:inputId, informationInputText:e.target.value}]
-                }
-            })
+            let identifiedElementIndex=prevCollectionInformationsToCreate.findIndex((prevCollectionInformationToCreate)=>prevCollectionInformationToCreate.informationInputId==inputId)
+            console.log('identifiedElement Index=>',identifiedElementIndex)
+            if(identifiedElementIndex!=-1){
+                console.log('if')
+                // return prevCollectionInformationsToCreate.splice(identifiedElementIndex,1,{informationInputId:inputId,informationInputText:e.target.value })
+                return prevCollectionInformationsToCreate.map((prevCollectionInformationToCreate)=>{
+                    const{informationInputId, informationInputText}=prevCollectionInformationToCreate
+                    if(informationInputId==inputId){
+                        return {...prevCollectionInformationToCreate,informationInputText:e.target.value}
+                    }
+                    else return prevCollectionInformationToCreate
+                })
+            } else {
+                console.log('else')
+                return [...prevCollectionInformationsToCreate,{informationInputId:inputId, informationInputText:e.target.value}]
+            }
         })
     }
 
