05044266a70a0f3ceda4bc1580686650b860f7d4
Thomassaiag <thomas.saiag@gmail.com>
2024-06-14 15:04:25 +0200

    discared db.js from gitignore

---
+++ server/config/db.js
@@ -0,0 +1,24 @@
+const Pool= require("pg").Pool
+require("dotenv").config({path: "../.env"});
+
+const pool = new Pool({
+    user:"postgres",
+    password: process.env.DBPASSWORD,
+    host: "localhost",
+    database: process.env.DBNAME
+})
+
+
+const connectToDatabase=async()=>{
+    try {
+        const client=await pool.connect()
+        console.log('Connected to database')
+        client.release()
+    } catch (error) {
+        console.error('Database connection error', error.stack)
+    }
+    
+
+}
+
+module.exports = {pool, connectToDatabase}
