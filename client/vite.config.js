d0c0a3b8b1d81a6db436262f70253b94aae25237
Thomassaiag <thomas.saiag@gmail.com>
2024-12-27 17:32:10 +0100

    update vite config to force build into distr

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Output directory for built files
+  },
   
   server:{
     port:14000
