e89af2ec2388d5bb900c4653cd68ab32a586d123
Thomassaiag <thomas.saiag@gmail.com>
2024-12-30 12:28:56 +0100

    updated main.yml to include tests

--- client/src/test/setup.js
+++ client/src/test/setup.js
@@ -1,6 +1,13 @@
-// src/test/setup.js
 import '@testing-library/jest-dom'
 import { vi } from 'vitest'
 
 // Add any global mocks or setup here
 global.fetch = vi.fn()
+
+
+const localStorageMock = {
+  getItem: vi.fn(),
+  setItem: vi.fn(),
+  clear: vi.fn()
+}
+global.localStorage = localStorageMock
